9cc9cb23aeccaee8dd73827a3b90f399327fe067
afkdude <kumarjordy12@gmail.com>
2024-03-28 22:18:33 +0530

    added some modifications

---
+++ script.js
@@ -0,0 +1,15 @@
+let computerChoice;
+
+function generateComputerChoice() {
+ 
+  let randomNumber = Math.random() * 3;
+
+
+  if (randomNumber > 0 && randomNumber <= 1) {
+    computerChoice = 'Bat';
+  } else if (randomNumber > 1 && randomNumber <= 2) {
+    computerChoice = 'Ball'
+  } else {
+    computerChoice = 'Stump'
+  }
+}
