692aca8059c76e5845049ea481fd3a942675046e
afkdude <kumarjordy12@gmail.com>
2024-03-28 14:55:26 +0530

    added a practice file

---
+++ practice.js
@@ -0,0 +1,39 @@
+// practice
+
+
+// let age = Math.random()
+// let gender = Math.random('Male', 'Female');
+// let age = 60;
+// let gender = 'female';
+// let finalDiscount;
+
+// if (age <= 5) {
+//   finalDiscount = 100;
+// } else if (gender === 'female') {
+//   finalDiscount = 50;
+// } else if (age <= 8) {
+//   finalDiscount = 50;
+// } else if (age >= 65) {
+//   finalDiscount = 30;
+
+// } else {
+//   finalDiscount = 0;
+// }
+// console.log(`your final discount is ${finalDiscount}`);
+
+//  topic - Functions
+
+function greet(name) {
+  console.log(`Hello  Mr ${name}`);
+}
+
+greet("Shubham Amrawat ");
+
+
+function age(old) {
+
+  console.log(`You are ${old} years old`);
+  
+}
+
+age(21)
